cb7dae6faa1b48a7ecd7161cfebb995b24a31b39
hxy1008 <1464252217@qq.com>
2023-04-18 22:16:41 +0800

    1

---
+++ 第二章 线性表/02线性表的基本操作/ADT_list_xianxing.c
@@ -0,0 +1,113 @@
+#define LIST_INIT_SIZE 100
+#define LIST_INCREMENT 10
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef enum Status{
+    success,fail,fatal,range_error
+}Status;
+
+typedef int ElemType;
+typedef struct SqList
+{
+    ElemType *elem;
+    int length;
+    int list_size;
+}SqList, *Ptr;
+typedef Ptr SqListPtr;
+//初始化
+Status List_Init(SqListPtr L)
+{
+    Status s = success;
+    L->list_size = LIST_INIT_SIZE;
+    L->elem = (ElemType *)malloc(sizeof(ElemType) * L->list_size);
+    if(L->elem == NULL){
+        s = fatal;
+    }
+    return s;
+}
+//按位置查找
+Status List_Retrival(SqListPtr L, int pos, ElemType *elem)
+{
+    Status s = range_error;
+    if(L){
+        if((pos -1 >= 0)&&(pos - 1 < L->length)){
+            *elem = L->elem[pos-1];
+            s = success;
+        }
+    }
+    else{
+        s = fatal;
+    }
+    return s;
+
+}
+// 按值查找
+Status List_Locate(SqListPtr L, ElemType elem, int *pos)
+{
+    Status s = range_error;
+    int i;
+    if(L){
+        for(i = 0;i < L->list_size;i++){
+            if(elem == L->elem[i]){
+                *pos = i+1;
+                s = success;
+                break;
+            }
+        }
+    }
+    else{
+        s = fatal;
+    }
+    return s;
+}
+// 插入操作
+Status List_Insert(SqListPtr L, int pos, ElemType elem)
+{
+    Status s = range_error;
+    int i;
+    if(L){
+        // if(L->length + 1 > L->list_size){
+        //     L->list_size = L->list_size + LIST_INCREMENT;
+        //     L->elem = realloc(L->elem, sizeof(ElemType) * L->list_size);
+        // }
+        if((pos - 1 >= 0)&&(pos - 1 <= L->length)){
+            if(L->length < L->list_size){
+                for(i=L->length - 1;i>=pos-1;i=i-1){
+                    L->elem[i+1] = L->elem[i];
+                    L->elem[pos - 1] = elem;
+                }
+                L->length++;
+                s = success;
+            }
+        }
+        else{
+            s = fail;
+        }
+    }
+    else{
+        s = fatal;
+    }
+    return s;
+}
+// 删除操作
+Status List_Delete(SqListPtr L, int pos)
+{
+    Status s = range_error;
+    if(L){
+        if((pos-1)>= 0&&(pos-1 < L->length)){
+            if(L->length > 0){
+                for(int i = pos;i < L->length;i++){
+                    L->elem[i-1] = L->elem[i];
+                }
+                s = success;
+                L->length--;
+            }
+        }
+        else s = fail;
+    }
+    else s = fatal;
+    return s;
+}
+
+
---
+++ 第二章 线性表/02线性表的基本操作/ADT_list_xianxing.h
@@ -0,0 +1,30 @@
+#ifndef _ADT_LIST_XIANXING_
+#define _ADT_LIST_XIANXING_
+#define LIST_INIT_SIZE 100
+#define LIST_INCREMENT 10
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef enum Status{
+    success,fail,fatal,range_error
+}Status;
+
+typedef int ElemType;
+typedef struct SqList
+{
+    ElemType *elem;
+    int length;
+    int list_size;
+}SqList, *Ptr;
+typedef Ptr SqListPtr;
+//初始化
+Status List_Init(SqListPtr L);
+//按位置查找
+Status List_Retrival(SqListPtr L, int pos, ElemType *elem);
+// 按值查找
+Status List_Locate(SqListPtr L, ElemType elem, int *pos);
+// 插入操作
+Status List_Insert(SqListPtr L, int pos, ElemType elem);
+// 删除操作
+Status List_Delete(SqListPtr L, int pos);
+#endif
